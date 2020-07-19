97510e5b1a9453ab05c24643f6c83ef4bdb7c723
Siyabonga Zwane <doctorszwane@gmail.com>
2020-07-19 13:49:36 +0200

    Added function comment for get_filters() function

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#defining a function to get filters
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
